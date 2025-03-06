649f0840eda18f2f999276c64b71391a43fab4d2
ahmad <ahmad.dstech@gmail.com>
2025-03-07 02:14:23 +0500

    final repo

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse, RedirectResponse, HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import uvicorn
+
+app = FastAPI()
+
+# Mount static files
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Initialize Jinja2Templates
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/home", response_class=HTMLResponse)
+async def home(request: Request):  # Request ko argument me lena zaroori hai
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/api/message", response_class=JSONResponse)
+async def message():
+    return {"message": "Hello from FastAPI"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
